335c8ae323c0a5c5e67e0a1e07a7aef49b9be5fa
Biyi123 <114018563+Biyi123@users.noreply.github.com>
2023-02-28 00:13:43 -0500

    Add files via upload

---
+++ Restaurant(eckert).py
@@ -0,0 +1,42 @@
+#Biyi Abass
+#CS175L-02
+#restaurant
+
+
+
+vegetarian = False
+vegan = False
+gluten_free = False
+
+response= str(input('Is anyone in your party a vegetarian? '))
+if response== 'yes':
+     vegetarian = True
+if response== 'no':
+     vegetarian = False
+
+response = str(input('Is anyone in your party a vegan? '))
+if response == 'yes':
+    vegan = True
+if response == 'no':
+    vegan = False
+
+response =str(input('Is anyone in your party gluten-free? '))
+if response == 'yes':
+    vegan = True
+if response == 'no':
+    
+    vegan = False
+
+print('Here are your restaurant choices. ')
+
+if not vegetarian and not vegan or glutenFree:
+    print('Joe\'s Gourment Burgers')
+    print('Main Street Pizza Company')
+    print('Corner Cafe')
+    print('The Chef\'s Kitchen')
+    print('Mama\'s Fine Italian')
+
+
+
+
+ 
